88b918b2be88223fdc108ae9e01259c566af8467
hajae <gkrhf@naver.com>
2024-08-21 16:53:39 +0900

    project structure, user Entity add

---
+++ bosukba/src/main/java/com/forloop/bosukba/domain/User.java
@@ -0,0 +1,19 @@
+package com.forloop.bosukba.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String password;
+
+    private String nickname;
+}
