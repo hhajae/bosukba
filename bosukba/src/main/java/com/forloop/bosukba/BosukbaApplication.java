8077bf6397a68926b721477d8f9ea975508ee01c
hajae <gkrhf@naver.com>
2024-08-21 16:38:46 +0900

    project init

---
+++ bosukba/src/main/java/com/forloop/bosukba/BosukbaApplication.java
@@ -0,0 +1,13 @@
+package com.forloop.bosukba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosukbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BosukbaApplication.class, args);
+	}
+
+}
---
+++ bosukba/src/test/java/com/forloop/bosukba/BosukbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forloop.bosukba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BosukbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
